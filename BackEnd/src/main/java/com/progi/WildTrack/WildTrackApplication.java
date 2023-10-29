8f4dda67a2de4e22a9c9595d9575701ae00d3448
Ivan Lisica <ivan.lisica@fer.hr>
2023-10-29 16:08:34 +0100

    Added backend

---
+++ BackEnd/src/main/java/com/progi/WildTrack/WildTrackApplication.java
@@ -0,0 +1,13 @@
+package com.progi.WildTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildTrackApplication.class, args);
+	}
+
+}
