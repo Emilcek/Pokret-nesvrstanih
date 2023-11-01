6e38aebd24de37fc47e74b31fdb5bc3ff22a7b2c
Sara Gašpar <sara.gaspar@dekod.hr>
2023-11-01 23:01:01 +0100

    dodan header i popravljen setup

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
