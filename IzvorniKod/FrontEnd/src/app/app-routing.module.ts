6c43320943420ccac0a75693496121001930dd22
ficov <fv54188@fer.hr>
2023-11-14 11:20:30 +0100

    Napravljena je komponenta za prikaz pristiglih zahtjeva adminu

--- IzvorniKod/FrontEnd/src/app/app-routing.module.ts
+++ IzvorniKod/FrontEnd/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserDataEditingComponent } from './user-data-editing/user-data-editing.component';
+import { UsersRequestsListComponent } from './users-requests-list/users-requests-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register' , component: RegisterComponent },
   { path: 'edit/:id', component: UserDataEditingComponent },
+  { path: 'requests', component: UsersRequestsListComponent },
 ];
 
 @NgModule({
