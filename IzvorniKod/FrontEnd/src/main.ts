4322f987aab90e834c442f501f31355c0d244c08
Emil <emil.prpic@fer.hr>
2023-11-16 00:25:46 +0100

    env fix

--- IzvorniKod/FrontEnd/src/main.ts
+++ IzvorniKod/FrontEnd/src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from "./environment/environment";
-import {enableProdMode} from "@angular/core";
+import { enableProdMode } from "@angular/core";
 
 if (environment.production) {
   enableProdMode();
