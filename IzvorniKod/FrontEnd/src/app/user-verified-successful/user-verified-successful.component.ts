8c5bc0f16c6cb54e0d82b9d925cdf409fc71c74c
Ivan <ic54032@fer.hr>
2023-11-17 10:08:11 +0100

    verifikacija mailom front- funckija onInit

--- IzvorniKod/FrontEnd/src/app/user-verified-successful/user-verified-successful.component.ts
+++ IzvorniKod/FrontEnd/src/app/user-verified-successful/user-verified-successful.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute  } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-user-verified-successful',
   templateUrl: './user-verified-successful.component.html',
   styleUrls: ['./user-verified-successful.component.css']
 })
-export class UserVerifiedSuccessfulComponent {
-  constructor(private router: Router) { }
+export class UserVerifiedSuccessfulComponent implements OnInit{
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
   goToLogin(){
     this.router.navigateByUrl("/login")
+  }
+
+  ngOnInit(){
+    const token = this.route.snapshot.paramMap.get("url")
+    this.http.get(environment.BASE_API_URL+"/auth?url="+token)
   }
 }
