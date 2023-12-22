18e99c93431823070bdd82c189877828fcd0481f
Ivan <ic54032@fer.hr>
2023-12-22 19:34:35 +0100

    register i login rade i daju sve errore koje trebaju davati

--- IzvorniKod/FrontEnd/src/app/app.module.ts
+++ IzvorniKod/FrontEnd/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { ExplorerProfileComponent } from './explorer-profile/explorer-profile.co
 import { UsersRequestsListComponent } from './users-requests-list/users-requests-list.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserVerifiedSuccessfulComponent } from './user-verified-successful/user-verified-successful.component';
-import { VerificationEmailSentComponent } from './verification-email-sent/verification-email-sent.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,8 +27,7 @@ import { VerificationEmailSentComponent } from './verification-email-sent/verifi
     ExplorerTasksComponent,
     ExplorerProfileComponent,
     UsersRequestsListComponent,
-    UsersListComponent,
-    VerificationEmailSentComponent
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
