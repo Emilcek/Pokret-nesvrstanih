70559e217d60b80befc2ad2b430ab7cd5524be1e
Emil <emil.prpic@fer.hr>
2023-12-30 13:19:01 +0100

    dodane postaje na inicijalizaciji

--- IzvorniKod/BackEnd/src/main/java/com/progi/WildTrack/DataInitializer.java
+++ IzvorniKod/BackEnd/src/main/java/com/progi/WildTrack/DataInitializer.java
@@ -2,12 +2,13 @@ package com.progi.WildTrack;
 
 import com.progi.WildTrack.domain.Description;
 import com.progi.WildTrack.service.ClientService;
+import com.progi.WildTrack.service.StationService;
 import com.progi.WildTrack.service.StatusService;
 import com.progi.WildTrack.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,16 +16,16 @@ import java.util.List;
 @Component
 public class DataInitializer {
 
-    @Autowired
-    private VehicleService vehicleService;
-
-    @Autowired
-    private StatusService statusService;
-
-    @Autowired
-    private ClientService clientService;
     private final List<String> vehicleTypes = Arrays.asList("hodanje", "dron", "auto", "brod", "cross motor", "helikopter");
     private final List<Description> statusTypes = Arrays.asList(Description.PENDING, Description.ACCEPTED, Description.REJECTED);
+    @Autowired
+    StationService stationService;
+    @Autowired
+    private VehicleService vehicleService;
+    @Autowired
+    private StatusService statusService;
+    @Autowired
+    private ClientService clientService;
 
     @EventListener
     public void appReady(ApplicationReadyEvent event) {
@@ -34,10 +35,8 @@ public class DataInitializer {
         for (Description statusType : statusTypes) {
             statusService.createStatus(statusType);
         }
+        stationService.createStations();
         clientService.createAdmin();
-
-
-
 
     }
 }
