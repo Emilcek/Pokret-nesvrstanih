6b4514ff0e18dd1257bbc620bb9fba720e86d441
Ivan <ic54032@fer.hr>
2024-01-04 20:35:25 +0100

    karta radi, treba namjestiti slanje akcije

--- IzvorniKod/FrontEnd/src/app/action-creation/action-creation.component.ts
+++ IzvorniKod/FrontEnd/src/app/action-creation/action-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component,AfterViewInit, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
@@ -16,7 +17,15 @@ export class ActionCreationComponent implements AfterViewInit, OnInit {
   isStationChosen: boolean = false;
   chosenStationName: any;
   chosenStationSurface: any;
-
+  startLocation:string="";
+  endLocation:string="";
+  markersGroup: any;
+  educatedForChosen:any;
+  task=new FormGroup({
+    station:new FormControl(''),
+    educatedFor:new FormControl(),
+    description:new FormControl('',Validators.required),
+  })
   ngOnInit() {
   }
 
@@ -33,6 +42,7 @@ export class ActionCreationComponent implements AfterViewInit, OnInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"></a>'
     });
 
+
     var customIcon = L.icon({
       iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png", // Specify the path to your custom icon image
       iconSize: [22, 32], // Set the size of the icon
@@ -40,16 +50,59 @@ export class ActionCreationComponent implements AfterViewInit, OnInit {
       popupAnchor: [0, -32] // Set the anchor point for popups (relative to its size)
     });
 
+    var baseMaps = {
+      "OpenStreetMap": tiles
+    }
+
     tiles.addTo(this.map);
+
+    // a layer group, used here like a container for markers
+     var mg = L.layerGroup();
+    this.map.addLayer(mg);
+
+    this.map.on('click', (e:any) => {
+      // get the count of currently displayed markers
+      var markersCount = mg.getLayers().length;
+      if (markersCount < 2) {
+        var marker = L.marker(e.latlng,{icon:customIcon}).addTo(mg);
+        if(markersCount!=1){
+          marker.bindPopup("Početna lokacija").openPopup();
+          this.startLocation=e.latlng.lat+","+e.latlng.lng
+        }else{
+          marker.bindPopup("Krajnja lokacija").openPopup();
+          this.endLocation=e.latlng.lat+","+e.latlng.lng
+        }
+
+        return;
+      }
+      console.log(this.startLocation)
+      // remove the markers when MARKERS_MAX is reached
+      mg.clearLayers();
+    });
+    console.log(this.startLocation)
+
   }
 
   ngAfterViewInit(): void {
     this.initMap();
   }
 
-  showOptions(){
-
+  addTask() {
+    if (this.task.valid && this.educatedForChosen!=undefined) {
+      this.tasks.push({"description": this.task.value.description, "endLocation": this.endLocation, "startLocation":this.startLocation,"educatedFor": this.educatedForChosen});
+    }
+    //console.log(this.tasks)
   }
 
+  saveEducatedFor(event:any){
+    console.log(event.target.value)
+    console.log(this.startLocation+","+this.endLocation)
+    this.educatedForChosen=event.target.value;
+    return event.target.value;
+  }
+
+  deleteFromTasks(task:any){
+    this.tasks.splice(this.tasks.indexOf(task),1)
+  }
 }
 
